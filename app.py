1c73f620c0f44e4630bfa0750f48e5036614e4f8
Alex <60197499+21st-centuryman@users.noreply.github.com>
2025-08-19 20:55:51 +0200

    Fix: port issue

--- app.py
+++ app.py
@@ -541,4 +541,4 @@ def mp32m4b(output_name, progress_messages):
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", debug=True, port=5001)
+    app.run(host="0.0.0.0", debug=True, port=5000)
